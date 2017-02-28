b2893fc46ec2dec597382886ae9b61f671764efb
marchabib <marc.habib@devinci.fr>
2017-02-28 22:19:54 +0100

    Done all the main fonctionality, just need to convert them into functions

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+
+    <form action="cible.php" method="post">
+
+        <input type="text" name="user" />
+        <input type="text" name="pass" />
+        <input type="submit" value="Valider" />
+
+    </form>
+
+
+  </body>
+</html>
