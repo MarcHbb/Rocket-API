13979e22a78cf121732c3674597a7d6b9344d359
marchabib <marc.habib@devinci.fr>
2017-03-01 02:03:13 +0100

    Splited the code into function and added style for the form

--- index.php
+++ index.php
@@ -1,19 +1,30 @@
 <!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="utf-8">
-    <title></title>
-  </head>
-  <body>
+<html >
+<head>
+  <meta charset="UTF-8">
+  <title></title>
 
-    <form action="cible.php" method="post">
-
-        <input type="text" name="user" />
-        <input type="password" name="pass" />
-        <input type="submit" value="Valider" />
-
-    </form>
+      <link rel="stylesheet" href="css/style.css">
 
 
-  </body>
+</head>
+
+<body>
+	<div class="container">
+		<h1>Welcome</h1>
+
+		<form class="form" action="cible.php" autocomplete="off" method="post">
+			<input type="text" name="user" autocomplete="off" placeholder="Username">
+			<input type="password" name="pass" autocomplete="off" placeholder="Password">
+			<button type="submit" id="login-button">Login</button>
+		</form>
+	</div>
+
+
+</div>
+<script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
+
+  <script src="script.js"></script>
+
+</body>
 </html>
